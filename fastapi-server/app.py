17895cb57372d393be276817aafc73b3fa9cf64f
Dominik Horvath <dominik.horvath.to@gmail.com>
2025-09-17 19:23:23 +0200

    add frontend

--- fastapi-server/app.py
+++ fastapi-server/app.py
@@ -1,12 +1,34 @@
-from fastapi import FastAPI, Query
+from fastapi import FastAPI, Query, Request
 from fastapi.responses import JSONResponse
 import llm_operator
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 from models import PharmacyQuestionAnswer
+import os
+import settings
 from dotenv import load_dotenv
+
 load_dotenv()
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
 
-@app.get("/")
+@app.get("/api/generate_pharmacy_qa", response_model=PharmacyQuestionAnswer)
 async def generate_pharmacy_qa(topic: str = Query(default=llm_operator.INPUT_FILE), previous_questions: list[str] = Query(default=[])) -> PharmacyQuestionAnswer:
 	return llm_operator.read_chapter_and_generate_qa(topic, previous_questions)
+
+
+@app.get("/", response_class=HTMLResponse)
+async def index(request: Request):
+    # list files in data directory and remove .json suffix
+    files = os.listdir(settings.PARSED_QUESTIONS_DIRECTORY)
+    topics = [f[:-5] for f in files if f.endswith(".json")]
+    return templates.TemplateResponse(request=request, name="index.html", context={"request": request, "topics": topics})
+
+
+@app.get("/topic/{topic}", response_class=HTMLResponse)
+async def index(request: Request, topic: str):
+
+    return templates.TemplateResponse(request=request, name="topic.html", context={"request": request, "topic": topic})
